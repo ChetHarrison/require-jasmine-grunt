8c3cba33e3edcdecc1259e5e07ef7f1992ece855
Chet Harrison <chet.harrison@gmail.com>
2014-08-27 16:31:22 -0700

    added coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,40 @@ module.exports = function(grunt) {
                 src: 'assets/**/*.js',
                 options: {
                     specs: 'spec/*spec.js',
-                    host: 'http://127.0.0.1:8000/',
+                    // host: 'http://127.0.0.1:8000/',
                     template: require('grunt-template-jasmine-requirejs'),
                     templateOptions: {
                         requireConfigFile: './assets/require-config.js'
                     }
                 }
+            },
+            coverage: {
+                src: 'assets/**/*.js',
+                options: {
+                    specs: 'spec/*spec.js',
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        coverage: 'coverage/coverage.json',
+                        report: 'coverage',
+                        thresholds: {
+                            lines: 75,
+                            statements: 75,
+                            branches: 75,
+                            functions: 90
+                        },
+                        // 1. don't replace src for the mixed-in template with instrumented sources
+                        replace: false,
+                        template: require('grunt-template-jasmine-requirejs'),
+                        templateOptions: {
+                            requireConfig: {
+                                requireConfigFile: './assets/require-config.js'
+                            }
+                        }
+                    }
+                }
             }
         }
+            
     });
 
     grunt.loadNpmTasks('grunt-contrib-jasmine');
